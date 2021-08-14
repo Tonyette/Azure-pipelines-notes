e99e9eb264f287a225dc4b0bea1c394e5cfb6bfa
tonyette <feyisayofamakinde@gmail.com>
2021-08-14 13:33:06 +0100

    first commit

---
+++ azurepipelinesnotes/bash.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#command for showing the first two arguments to the bash script
+#and the number of arguement pushed to it
+#plus the exit status of the arguments
+#echo $1 $2
+
+#echo $#
+
+#echo $?
+
+
+
